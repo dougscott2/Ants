d728d461793083e9e17912a28d52575e124fd151
DrScott <dougscott2@gmail.com>
2015-12-02 11:20:07 -0500

    that's how you get ants

---
+++ src/com/theironyard/Ant.java
@@ -0,0 +1,15 @@
+package com.theironyard;
+
+/**
+ * Created by DrScott on 12/2/15.
+ */
+public class Ant {
+    double x;
+    double y;
+
+
+    public Ant(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
